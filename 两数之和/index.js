229209f818ef14b14b303c346b092a7a0344ea6c
GuanLola <3079756620@qq.com>
2021-11-09 10:15:11 +0800

    两数之和

---
+++ 两数之和/index.js
@@ -0,0 +1,29 @@
+const nums = [2, 7, 11, 13];
+const target = 9;
+
+// 双重循环
+
+/* var twoSum = function(nums, target) {
+
+  for (let i = 0; i < nums.length; i++) {
+
+    for (let j = i + 1; j < nums.length; j++) {
+
+      if (nums[i] + nums[j] === target && i !== j) {
+
+        return [i, j]
+
+      }
+
+    }
+
+  }
+} */
+
+// 两数之和
+
+var twoSum = function(nums, target) {};
+
+const result = twoSum(nums, target);
+
+console.log(result);
